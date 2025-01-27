8f20d7c87b9cb194441d879a85a3552aa7fe9c24
Olof Appelblad <117285847+knzoon@users.noreply.github.com>
2025-01-27 23:15:49 +0100

    First try reading takeovers from db

--- exportfeed/export.py
+++ exportfeed/export.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from fastapi import FastAPI
+import feedRepository
+import json
 
 app = FastAPI()
 
@@ -8,4 +10,11 @@ app = FastAPI()
 def read_feed_starting(start_time: datetime = None):
     if start_time:
         return {"takeovers": f"Takeovers after {start_time}"}
-    return {"takeovers": "All takeovers"}
+
+    db_rows = feedRepository.read_feed_from()
+    result = []
+
+    for (takeover_time, original_takeover) in db_rows:
+        result.append(json.loads(original_takeover))
+
+    return result
---
+++ exportfeed/feedRepository.py
@@ -0,0 +1,45 @@
+import mariadb
+import sys
+
+def getConnection():
+    try:
+        conn = mariadb.connect(
+            user="knzoonApp",
+            password="appApa",
+            host="database",
+            port=3306,
+            database="knzoon"
+
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+    
+    return conn
+
+
+def read_feed_from():
+    with getConnection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("select takeover_time, original_takeover from improved_feed_item order by takeover_time limit 5")
+            return cur.fetchall()
+
+
+# def getLatestReadInfo(conn):
+#     cur = conn.cursor()
+#     feedid = 1
+#     cur.execute("select last_time, last_zone_id, last_highest_order from external_feed_read where id = ?", (feedid,))
+#
+#     for (last_time, last_zone_id, last_highest_order) in cur:
+#         pass
+#     return (last_time, last_zone_id, last_highest_order)
+#
+# def insertTakever(conn, newhighestorder, takeoverTime, feedItemAsString):
+#     cur = conn.cursor()
+#     sql = "insert into improved_feed_item (id, order_number, takeover_time, original_takeover) values (?, ?, ?, ?)"
+#     cur.execute(sql, (str(uuid.uuid4()), newhighestorder, takeoverTime, feedItemAsString))
+#
+# def updateLatestReadInfo(conn, lastTime, lastZoneId, lastHighestOrder):
+#     cur = conn.cursor()
+#     sql = "update external_feed_read set last_time = ?, last_zone_id = ?, last_highest_order = ? where id = 1"
+#     cur.execute(sql, (lastTime, lastZoneId, lastHighestOrder))
